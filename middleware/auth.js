e079553be1fd559bea95d328d0d8fc27a58591e6
Ziad Mohamed Ismail <ziadmhmd991@gmail.com>
2023-09-19 14:03:01 +0300

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+import jwt from 'jsonwebtoken';
 const auth = async (req,res,next) => {
     try {
         const token = req.headers.authorization.split(" ")[1];
